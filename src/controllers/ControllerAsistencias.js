5bc0f7a3691018ebb8f695c9278d0e10d2693b7c
AlexGarcia53 <alex.garcia228721@potros.itson.edu.mx>
2023-11-06 23:01:13 -0700

    cambios en controlador para cargar archivo
    se cambio la forma de procesar el archivo

--- src/controllers/ControllerAsistencias.js
+++ src/controllers/ControllerAsistencias.js
@@ -4,53 +4,62 @@ const csvtojson = require('csvtojson')
 
 class ContollerAsistencia {
     static async cargarAsistencia(req, res) {
+      try {
+        const grupo = req.body.grupo;
+        const filePath = req.file.path;
+        const asistenciasProcesadas = await ContollerAsistencia.procesarArchivoCSV(filePath, grupo);
+  
+        const archivoCargado = await asistenciasDAO.cargar(asistenciasProcesadas);
+        res.status(201).json(archivoCargado);
+      } catch (err) {
+        res.status(500).json({ error: 'No se pudo cargar o procesar el archivo' });
+      }
+    }
+  
+    static async procesarArchivoCSV(filePath, grupo) {
         try {
-            console.log(req.file.path);
-            const asistenciasProcesadas = await ContollerAsistencia.procesarArchivoCSV(req.file.path);
-           
-            const archivoCargado = await asistenciasDAO.cargar(asistenciasProcesadas);
-            res.status(201).json(archivoCargado);
+            const asistencias = await csvtojson({
+                noheader: true,
+                headers: ['Names', 'Attendance', 'ArrivalTime'],
+            }).fromFile(filePath);
+    
+            
+            const fechas = [{ fecha: new Date() }]; 
+    
+            const asistenciasProcesadas = [];
+            asistencias.slice(3).forEach((asistencia) => {
+                const tiempoLlegada = asistencia.ArrivalTime.split(' ');
+                const horaLlegada = tiempoLlegada[0];
+                const duracion = tiempoLlegada[1];
+                const horaSalida = tiempoLlegada[2];
+    
+                const processedAsistencia = {
+                    nombreEstudiante: asistencia.Names,
+                    asistio: asistencia.Attendance === '✔',
+                    horaAsistencia: {
+                        llegada: horaLlegada,
+                        duracion: duracion,
+                        salida: horaSalida,
+                    },
+                };
+    
+                asistenciasProcesadas.push(processedAsistencia);
+            });
+    
+            const dataToSave = {
+                grupo: grupo,
+                fechas: fechas.map((fecha) => {
+                    return { ...fecha, asistencias: asistenciasProcesadas };
+                }),
+            };
+    
+            return [dataToSave];
         } catch (err) {
-            res.status(500).json({ error: 'No se pudo cargar o procesar el archivo' });
+            console.error(err);
+            throw err;
         }
     }
-
-
-    static async procesarArchivoCSV(filePath) { 
-            try {
-                const asistencias = await csvtojson({
-                    noheader: true, 
-                    headers: ['Names', 'Attendance', 'ArrivalTime'] 
-                }).fromFile(filePath);
-    
-                const datosAsistencias = asistencias.slice(3);
-                const asistenciasProcesadas = datosAsistencias.map(asistencia => {
-                    const tiempoLlegada = asistencia.ArrivalTime.split(' ');
-                    const horaLlegada = tiempoLlegada[0];
-                    const duracion = tiempoLlegada[1];
-                    const horaSalida = tiempoLlegada[2];
-    
-                    return {
-                        nombreEstudiante: asistencia.Names,
-                        fechaClase: new Date(),
-                        asistio: asistencia.Attendance === "✔",
-                        horaAsistencia: {
-                            llegada: horaLlegada,
-                            duracion: duracion,
-                            salida: horaSalida
-                        }
-                    };
-                });
-                return asistenciasProcesadas;
-            } catch (err) {
-                console.error(err);
-                throw err;
-            }
-        }
+  
 }
-
-
-
-
 
 module.exports = ContollerAsistencia;
