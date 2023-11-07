944806e88759a1565ef6de2c7ef7c615a477b1fe
Juan-Sotelo <juan.sotelo229391@potros.itson.edu.mx>
2023-11-06 23:11:31 -0700

    Consultas para visualizar asistencias
    Se añadieron 2 consultas para visualización de asistencias, por grupo y por alumno

---
+++ src/controllers/ControllerConsultas.js
@@ -0,0 +1,27 @@
+const asistenciasDAO = require('../data/AsistenciasDAO')
+require('../data/database');
+
+class ControllerConsulta {
+    static async asistenciasAlumno(req, res) {
+        try{
+            console.log(req.body.grupo+", "+req.body.alumno)
+            const consulta= await asistenciasDAO.consultarAsistenciasAlumno(req.body.grupo, req.body.alumno);
+            res.status(201).json(consulta);
+        } catch (err) {
+            console.error(err)
+            res.status(500).json({error: "No se pudieron recuperar las asistencias"})
+        }
+    }
+
+    static async asistenciasGrupo(req, res) {
+        try{
+            const consulta= await asistenciasDAO.consultarPorGrupo(req.body.grupo)
+            res.status(201).json(consulta);
+        } catch (err) {
+            console.error(err)
+            res.status(500).json({error: "No se pudieron recuperar las asistencias"})
+        }
+    }
+}
+
+module.exports = ControllerConsulta;
--- src/data/AsistenciasDAO.js
+++ src/data/AsistenciasDAO.js
@@ -12,6 +12,57 @@ class AsistenciaDAO {
             throw err;
         }
     }
+
+    static async consultarPorGrupo(nombreGrupo){
+        try{
+            const consulta= await Asistencia.findOne({grupo: nombreGrupo});
+            console.log(consulta)
+            return consulta
+        } catch (err) {
+            console.error(err)
+            throw err
+        }
+    }
+
+    static async consultarAsistenciasAlumno(nombreGrupo, alumno){
+        try{
+            const resultados= await Asistencia.aggregate([
+                {
+                    $match: {
+                        grupo: nombreGrupo
+                    }
+                },
+                {
+                    $unwind: "$fechas"
+                },
+                {
+                    $unwind: "$fechas.asistencias"
+                },
+                {
+                    $match: {
+                        "fechas.asistencias.nombreEstudiante": alumno,
+                        "fechas.asistencias.asistio":true
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        fechas: { $addToSet: "$fechas.fecha" }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        fechas:  1
+                    }
+                }
+            ])
+            console.log(resultados);
+            return resultados;
+        } catch(err) {
+            throw err;
+        }  
+    }
 }
 
 module.exports = AsistenciaDAO;
