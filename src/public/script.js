2c13df39b278752e8ff1bb0693611795c691db23
AlexGarcia53 <alex.garcia228721@potros.itson.edu.mx>
2023-11-16 15:11:47 -0700

    integración de front y back
    integración para carga de archivo, cambios en backend y frontend

--- src/public/script.js
+++ src/public/script.js
@@ -2,6 +2,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const dropArea = document.getElementById('dropArea');
     const fileInput = document.getElementById('fileInput');
     const browseButton = document.getElementById('browseButton');
+    const uploadButton = document.getElementById('uploadButton');
+    const grupoInput = document.getElementById('grupoInput');
+    
+    browseButton.addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener('change', () => {
+    
+        console.log('Archivo seleccionado:', fileInput.files[0].name);
+    });
+
+    uploadButton.addEventListener('click', () => {
+
+        if (fileInput.files.length > 0 && grupoInput.value.trim() !== '') {
+            const formData = new FormData();
+            formData.append('grupo', grupoInput.value.trim()); 
+            formData.append('csvFile', fileInput.files[0]);
+            
+
+            const baseURL = 'http://localhost:8721';  
+            fetch(`${baseURL}/asistencias/nueva`, {
+              method: 'POST',
+              body: formData
+            })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Archivo y grupo enviado al servidor', response);
+                    resetDropArea();
+                    alert('¡Archivo subido correctamente!');
+                } else {
+                    console.error('Error al enviar el archivo y el grupo', response.statusText);
+                    return response.json(); // Lee el cuerpo de la respuesta
+                }
+            })
+            .then(data => {
+                if (data && data.error) {
+                    console.error('Error adicional:', data.error);
+                    alert('Error al subir el archivo: ' + data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Error al enviar el archivo y el grupo', error)
+            });
+        } else {
+            console.error('Selecciona un archivo y ingresa un grupo antes de intentar subirlo.');
+        }
+
+    });
 
     dropArea.addEventListener('dragover', (e) => {
         e.preventDefault();
@@ -37,4 +86,19 @@ document.addEventListener('DOMContentLoaded', () => {
         dropArea.appendChild(icon);
         dropArea.appendChild(document.createTextNode(file.name));
     }
+
+    function resetDropArea() {
+        grupoInput.value = '';
+        dropArea.style.backgroundColor = 'white';
+        dropArea.innerHTML = `
+            <input type="file" id="fileInput" style="display: none">
+            <button id="browseButton" class="browse-button">O selecciona un archivo</button>
+            <div class="drop-text">📁 Arrastra y suelta archivos aquí</div>
+        `;
+        const newBrowseButton = document.getElementById('browseButton');
+        newBrowseButton.addEventListener('click', () => {
+            fileInput.click();
+        });
+    }
 });
+
