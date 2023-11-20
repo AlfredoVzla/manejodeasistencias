0c7c6ac36396e376f58cb904a5f6ea900710488d
AlexGarcia53 <alex.garcia228721@potros.itson.edu.mx>
2023-11-20 13:23:12 -0700

    corrección de bug
    cambios en middleware para validar formato de archivo

--- src/utils/validarFormato.js
+++ src/utils/validarFormato.js
@@ -10,17 +10,24 @@ function validarCSV(req, res, next) {
         return res.status(400).json({ error: 'El archivo no es un archivo .csv' });
     }
 
-    // Leer el archivo de manera síncrona para validar el formato
-    const fileContent = fs.readFileSync(file.path, 'utf8');
-    const lines = fileContent.split('\n');
+    fs.readFile(file.path, 'utf8', (err, fileContent) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error al leer el archivo' });
+        }
 
-    // Validar la tercera línea (línea de encabezados)
-    const expectedHeader = 'Names,2020-09-10 11:29,Arrival time';
-    if (lines[2].trim() !== expectedHeader) {
-        return res.status(400).json({ error: 'El archivo no tiene el formato correcto' });
-    }
+        const lines = fileContent.split('\n');
 
-    next();
+        if (!lines[0].startsWith('Attendance for:')) {
+            return res.status(400).json({ error: 'El archivo no tiene el formato correcto' });
+        }
+
+        const fileColumns = lines[2].trim().split(',');
+
+        if (fileColumns.length < 3) {
+            return res.status(400).json({ error: 'El archivo no tiene el formato correcto' });
+        }
+        next();
+    });
 }
 
 module.exports = { validarCSV };
