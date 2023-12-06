f5c8a6f0c7949e40bf6941dedfb6d2346c40811f
Juan-Sotelo <juan.sotelo229391@potros.itson.edu.mx>
2023-12-05 17:27:39 -0700

    Funcionalidad modificar curso
    Se agregó la funcionalidad de modificar curso ya terminada.

--- src/routers/RouterCursos.js
+++ src/routers/RouterCursos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controllerCurso = require('../controllers/ControllerCursos');
 
 router.post('/nuevo', controllerCurso.agregarCurso);
+router.put('/modificar', controllerCurso.actualizarCurso);
 
 module.exports = router;
